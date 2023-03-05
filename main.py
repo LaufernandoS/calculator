40364ffa67402b7b1e2b3a3b7701d80c4ba2d9cc
LaufernandoS <112432546+LaufernandoS@users.noreply.github.com>
2023-03-05 18:47:45 -0300

    Calculator Frame

---
+++ main.py
@@ -0,0 +1,10 @@
+# Importing libraries
+import math
+from tkinter import *
+from tkinter import ttk
+
+# Creating the calculator window
+window = Tk()
+window.title("Calculator")
+window.geometry("360x640")
+window.mainloop()
